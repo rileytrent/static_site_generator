e395039cffa066d7713f4647ec9adf4020c8f545
rileytrent <rileytrent8@gmail.com>
2025-04-26 10:51:28 -0700

    change files back to normal

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from textnode import TextNode
 
 
-# will this add? https://github.com/rileytrent/static_site_generator.git
 def main():
     test=TextNode("Testing","bold","www.gooba.com")
     print(test)
